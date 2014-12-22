d2a7ab345b243e2eb59495be71662b52820c9c31
Tom <tomfperegrine@googlemail.com>
2014-12-22 00:23:16 +0000

    Sidebar Animation

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,24 +1,55 @@
 var sidebarOut = true;
+var contentWidth = "83.7%";
+var contentResize = true;
 
 $(document).ready(function(){
-   $("#b").click(function(){
-       if (sidebarOut){
-           console.log("Hiding");
-            $("#NavBox").hide("slide", { direction: "left" }, 500,function(){
-                $("#Content").animate({width: "99%"},500);
+    //
+    //MultiScreen Sizing
+    if(screen.innerWidth <= 800){
+        
+    }
+    
+    $("#Titlebox").click(function(){
+        if (sidebarOut){
+            $("#SideBar").hide("slide", { direction: "left" }, 500,function(){
+                if (contentResize){
+                    $("#Content").animate({width: "99%"},500);
+                }
                 sidebarOut = false;
             });
-       }
-       else{
-           console.log("Showing");
-           $("#Content").animate({width: percentage(screen.width, 78)},500,function(){
-                $("#NavBox").show("slide", { direction: "left" }, 500);
-           });
-           sidebarOut = true;
-       }
-   }); 
+        }
+        else{
+            if (contentResize) {
+                $("#Content").animate({width: contentWidth},500,function(){
+                    $("#SideBar").show("slide", { direction: "left" }, 500);
+                });
+            }
+            else {
+                $("#SideBar").show("slide", { direction: "left" }, 500);
+            }
+            sidebarOut = true;
+        }
+    }); 
 });
 
 function percentage (total, percent){
     return (total / 100) * percent;
+}
+
+function checkSidebar() {
+    if (sidebarOut) {
+        $("#SideBar").show("slide", { direction: "left" }, 500,function(){
+            if (contentResize){
+                $("#Content").animate({width: contentWidth},500);
+            }
+        });
+    }
+    else {
+        $("#SideBar").hide("slide", { direction: "left" }, 500,function(){
+            if (contentResize){
+                $("#Content").animate({width: "99%"},500);
+            }
+            sidebarOut = false;
+        });
+    }
 }
